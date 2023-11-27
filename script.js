73665686b6951c6e5e6024e2e4ab7e054b96a9fb
ayrton <ayrtonundec@gmail.com>
2023-11-26 23:17:59 -0300

    script agregado

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let botones = document.querySelectorAll('button');
+
+botones.forEach((boton) => {
+  boton.addEventListener('click', () => {
+    alert('¡Gracias por su visita!');
+  });
+});
+
+
+
